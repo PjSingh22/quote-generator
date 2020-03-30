32c8719fec32d5afe188cc817f53ce236e191ced
Prabhjot Singh <pjsingh1359@outlook.com>
2020-03-30 09:13:52 -0700

    initial state on mount now shows first quote from the list

--- src/components/quote-box/quote-box.component.jsx
+++ src/components/quote-box/quote-box.component.jsx
@@ -5,7 +5,10 @@ class QuoteBox extends React.Component {
   constructor () {
     super()
     this.state = {
-      quotes: []
+      quotes: [],
+      currentQuote: '',
+      currentAuthor: '',
+      color: null
     }
   }
 
@@ -13,27 +16,46 @@ class QuoteBox extends React.Component {
     fetch('https://cors-anywhere.herokuapp.com/https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
       .then(res => res.json())
       .then(res => this.setState({
-        quotes: res.quotes
+        quotes: res.quotes,
+        currentQuote: res.quotes.map(item => item.quote)[0],
+        currentAuthor: res.quotes.map(item => item.author)[0]
       }))
   }
 
   render () {
-    const { quotes } = this.state
+    const { quotes, currentAuthor, currentQuote } = this.state
+
+    const colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', '#472E32', '#BDBB99', '#77B1A9', '#73A857']
+    const randomColor = Math.floor(Math.random() * colors.length)
+
     const randomRes = Math.floor(Math.random() * quotes.length)
-    console.log(quotes.map(item => item.quote)[randomRes])
-    console.log(quotes.map(item => item.author)[randomRes])
+
+    const divStyle = {
+      '.text': colors[randomColor],
+      button: colors[randomColor]
+    }
+
+    const getRandomQuote = () => {
+      const theQuote = quotes.map(item => item.quote)[randomRes]
+      const theAuthor = quotes.map(item => item.author)[randomRes]
+      return this.setState({
+        currentQuote: theQuote,
+        currentAuthor: theAuthor,
+        color: divStyle
+      })
+    }
 
     return (
       <div id="wrapper">
         <div id="quote-box">
           <div className="quote-text">
-            <i className="fa fa-quote-left">`&quot;`</i><span id="text">{quotes.map(item => item.quote)[randomRes]}</span>
+            <i className="fa fa-quote-left">`&quot;`</i><span id="text">{currentQuote}</span>
           </div>
           <div className="quote-author">
-          - <span id="author">{quotes.map(item => item.author)[randomRes]}</span>
+          - <span id="author">{currentAuthor}</span>
           </div>
           <div className="buttons">
-            <button className="button" id="new-quote">New quote</button>
+            <button className="button" id="new-quote" style={{ backgroundColor: colors[randomColor] }} onClick={getRandomQuote}>New quote</button>
           </div>
         </div>
       </div>
